41e5c0af720e152f998f9903345c6c88d059a770
MarcoDL99 <marcodl722@gmail.com>
2022-05-24 23:21:49 +0200

    Init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'HCI',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
